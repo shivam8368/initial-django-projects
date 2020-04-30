8d3cae284793d0dea4efe29b3b8374c11411654e
shivam kushwaha <shivamk8368@gmail.com>
2020-04-29 20:09:09 -0400

    todo list app

---
+++ 07projectseven/mywebsite/todo/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render , redirect
+from .forms import TodoForm
+from .models import Todo
+from django.views.decorators.http import require_POST
+
+
+def index(request):
+    mytodo = Todo.objects.order_by('id')
+    form = TodoForm()
+    context = {
+              'mytodo' : mytodo ,
+              'form'  :  form
+               }
+    return render(request , 'todo/index.html' ,context )
+
+@require_POST
+def addNewTodo(request):
+    form = TodoForm(request.POST)
+    if form.is_valid():
+        my_new_todo = Todo(todotext = request.POST['text'])
+        my_new_todo.save()
+
+    return redirect('index')
+
+
+def CompleteTodo(request , todo_id):
+    mytodo = Todo.objects.get(pk= todo_id)
+    mytodo.complete = True
+    mytodo.save()
+    return redirect('index')
+
+def IncompleteTodo(request , todo_id):
+    mytodo = Todo.objects.get(pk= todo_id)
+    mytodo.complete = False
+    mytodo.save()
+    return redirect('index')
+
+def deleteTodo(request):
+    Todo.objects.filter(complete__exact = True).delete()
+    return redirect('index')
+
+def resetTodo(request):
+    Todo.objects.all().delete()
+    return redirect('index')
